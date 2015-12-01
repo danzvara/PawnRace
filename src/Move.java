d78c87c434cdd5842fb0cb9149690b5546085aa0
dz2615 <dz2615@ic.ac.uk>
2015-12-01 02:00:23 +0000

    Square class finished, Move under construction

--- src/Move.java
+++ src/Move.java
@@ -2,4 +2,27 @@
  * Created by daniel on 01/12/15.
  */
 public class Move {
+
+    private Square from;
+    private Square to;
+    private boolean captured;
+
+    public Move(Square inFrom, Square inTo, boolean inCaptured) {
+        from = inFrom;
+        to = inTo;
+        captured = inCaptured;
+    }
+
+    public Square getFrom() {
+        return from;
+    }
+
+    public Square getTo() {
+        return to;
+    }
+
+    public boolean isCaptured() {
+        return captured;
+    }
+
 }
--- src/Square.java
+++ src/Square.java
@@ -2,4 +2,33 @@
  * Created by daniel on 01/12/15.
  */
 public class Square {
+
+    public enum COLOR {
+        BLACK, WHITE, NONE
+    }
+
+    private int y;
+    private int x;
+    private COLOR color;
+
+    public Square(int inx, int iny) {
+        x = inx;
+        y = iny;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public COLOR occupiedBy() {
+        return color;
+    }
+
+    public void setOccupier(COLOR new_color) {
+        color = new_color;
+    }
 }
